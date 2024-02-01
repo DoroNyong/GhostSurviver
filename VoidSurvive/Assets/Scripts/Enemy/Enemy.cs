650cf207e001d49343b2d7621e3ba1f979f2a702
DoroNyong <pride5623@naver.com>
2024-02-02 02:58:00 +0900

    [Update] 적군이 플레이어를 추적하는 기능 추가 및 플레이어와 충돌 이벤트 구현

--- VoidSurvive/Assets/Scripts/Enemy/Enemy.cs
+++ VoidSurvive/Assets/Scripts/Enemy/Enemy.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int hp = 3;
+    public float speed = 3f;
+
+    public GameObject Player = null;
+
+    public Animator animator;
+
+    public bool isDead = false;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!isDead)
+        {
+            Vector3 lookPlayer = (Player.transform.position - gameObject.transform.position).normalized;
+            Vector3 enemyMoveDir = lookPlayer;
+
+            Quaternion newRoataion = Quaternion.LookRotation(enemyMoveDir);
+            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, newRoataion, 2f * Time.deltaTime);
+
+            transform.position += enemyMoveDir * Time.deltaTime * 4f;
+        }
+    }
+
+    private void Hit()
+    {
+        hp -= 1;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isDead = true;
+            StartCoroutine("Dead");
+        }
+    }
+
+    IEnumerator Dead()
+    {
+        animator.SetTrigger("isDead");
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length + 0.01f);
+        Destroy(gameObject);
     }
 }
